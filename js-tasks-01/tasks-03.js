7837e24c45af8014ec356f3845784744c4250b0c
kostyaKobel1 <kostp72323@gmail.com>
2025-10-24 13:27:42 +0300

    Added a new tasks at JavaScript

---
+++ js-tasks-01/tasks-03.js
@@ -0,0 +1,77 @@
+// 1 Завдання
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+// Write a function calculating circumference of a Circle.
+function circleCircumference(circle) {
+  const circumference = 2 * Math.PI * circle.radius;
+  return Number(circumference.toFixed(6));
+}
+
+// 2 Завдання
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+//Create an array (which you will eventually return). Then, traverse obj, checking each key and value. 
+// If the length of the key is equal to 5, then push the key to your array. 
+// Separately, if the length of the value is equal to 5, then push the value to your array.
+// At the end, return your array.
+
+function giveMeFive(obj){
+  const arr = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+      }
+    if (obj[key].length === 5) {
+      arr.push(obj[key])
+    }
+  }
+  return obj
+}
+
+// 3 Завдання
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+// The function buildFun will return an array of functions.
+// The single parameter accepted by buildFun is the number of elements N of the array returned.
+
+function buildFun(n) {
+    const res = [1,2,3,4,5,6,7,8,9,10]
+  
+    function createFunction(index) {
+        return function () {
+            return index;
+        };
+    };
+    for (let i = 0; i < n; i++) {
+        res.push(
+            createFunction(i)
+        )
+    }
+    return res
+}
+
+// 4 Завдання
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+// Define the following classes that inherit from Animal.
+class Shark extends Animal {
+  constructor(name,age,status) {
+    super(name,age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor (name,age,status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce () {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
